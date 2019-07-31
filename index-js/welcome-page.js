7732bf6fc625356e7938c81eddbcdcacd8d31799
michael <747391119@qq.com>
2019-07-31 10:06:00 +0800

    音乐主页欢迎页及登录页

---
+++ index-js/welcome-page.js
@@ -0,0 +1,39 @@
+{
+    let view={
+        el:'.welcome-page',
+        template:`
+        <div class="welcome-head">
+            <p>Walkthrough</p>
+            <a id="welcome-skip">skip</a>
+        </div>
+        <div class="welcome-content">
+            <div class="welcome-icon"><img src="img/music-icon.png" alt="图片加载失败"></div>
+            <p class="welcome-icon-bottom">Musical</p>
+            <p class="welcome-text">Welcome</p>
+            <p class="welcome-text-bottom">Nice to meet you </br> in the world of music</p>
+        </div>
+        `,
+        render(data){
+            $(this.el).html(this.template)
+        }
+    }
+
+    let model={}
+
+    let controler={
+        init(view,model){
+            this.view = view;
+            this.model = model;
+            this.view.render();
+            this.pageOff();
+        },
+
+        pageOff(){
+            $(view.el).find('#welcome-skip').click(()=>{
+                $(view.el).fadeOut();
+            });
+        }
+    }
+    
+    controler.init(view,model);
+}
