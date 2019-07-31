f027e3d53475f66dc672406ecf1e1f8378498bd0
michael <747391119@qq.com>
2019-07-31 11:53:14 +0800

    完善页面背景模糊及细节

--- index-js/welcome-page.js
+++ index-js/welcome-page.js
@@ -2,16 +2,19 @@
     let view={
         el:'.welcome-page',
         template:`
-        <div class="welcome-head">
-            <p>Walkthrough</p>
-            <a id="welcome-skip">skip</a>
-        </div>
-        <div class="welcome-content">
-            <div class="welcome-icon"><img src="img/music-icon.png" alt="图片加载失败"></div>
-            <p class="welcome-icon-bottom">Musical</p>
-            <p class="welcome-text">Welcome</p>
-            <p class="welcome-text-bottom">Nice to meet you </br> in the world of music</p>
-        </div>
+        <div class="welcome-bg welcome-bg-blur"></div>
+        <div class="welcome-wrap">
+            <div class="welcome-head">
+                <p>Walkthrough</p>
+                <a id="welcome-skip">skip</a>
+            </div>
+            <div class="welcome-content">
+                <div class="welcome-icon"><img src="img/music-icon.png" alt="图片加载失败"></div>
+                <p class="welcome-icon-bottom">Musical</p>
+                <p class="welcome-text">Welcome</p>
+                <p class="welcome-text-bottom">Nice to meet you </br> in the world of music</p>
+            </div>
+        </div>    
         `,
         render(data){
             $(this.el).html(this.template)
